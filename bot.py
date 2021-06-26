29c00b912217cb473c8034843a61e34ec40790b3
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-06-26 13:06:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ CHANNEL = os.environ.get("CHANNEL","")
 app = Client(
         "pdfbot",
         bot_token=TOKEN,
-        api_id=APP_ID,
+        api_id=API_ID,
         api_hash=API_HASH)
 
 LIST = {}
